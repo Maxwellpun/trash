23e0693dacfc791a7d206949545cd7bb11cd22a7
Maxwellpun <108311761+Maxwellpun@users.noreply.github.com>
2022-10-07 17:52:09 +0700

    Add files via upload

---
+++ ex_loadcell_20kg_combo/ex_loadcell_20kg_combo.ino
@@ -0,0 +1,98 @@
+#include "HX711.h"
+#include <ArduinoJson.h>
+#include <SPI.h>
+#include <Ethernet.h>
+#include <PubSubClient.h>
+#include <String.h>
+
+byte mac[] = {0x12, 0x3A, 0x00, 0xBB, 0x00, 0x01};
+EthernetClient ethClient;
+PubSubClient mqttClient(ethClient);
+const char* mqttServer = "mqtt-io.kku.ac.th";
+const int mqttPort = 1991;
+const char* mqttUser = "trash";
+const char* mqttPass = "1q2w3e4r@trash";
+const char* mqttTopic = "smartcity/trash/00001";
+char* mqttPayload = "";
+
+float calibration_factor =54742; 
+#define zero_factor 8313974
+#define DOUT  8
+#define CLK   7
+#define DEC_POINT  2
+
+float offset=0;
+float get_units_kg();
+
+HX711 scale(DOUT, CLK);
+
+void setup() 
+{
+  Serial.begin(9600);
+    Serial.println("Ethernet initiation, Just wait a minute.");
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Don't receive ip from dhcp server.");
+    if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+      Serial.println("Ethernet shield connection error!");
+    } else if (Ethernet.linkStatus() == LinkOFF) {
+      Serial.println("Cable is not connected.");
+    } else {
+      Serial.println("Error!");
+    }
+    while(true){
+      delay(1);
+    }
+  }
+  if(Ethernet.linkStatus() == 0) { 
+    print_ip_details();
+  } else {
+    Serial.println("Error!");
+  }
+  mqttClient.setServer(mqttServer,mqttPort);
+  mqttClient.connect("arduinoClient", mqttUser, mqttPass);
+  Serial.println("Load Cell");
+  scale.set_scale(calibration_factor); 
+  scale.set_offset(zero_factor);   
+}
+void loop() 
+{ 
+  
+  DynamicJsonDocument doc(1024);
+  
+  Serial.print("Reading: ");
+  String data = String(get_units_kg()+offset, DEC_POINT);
+  Serial.print(data);
+  Serial.println(" kg");
+  delay(60000);
+
+  String jData =  String("{\"weight\": ") + data + String("}");
+  char buf[100];
+  jData.toCharArray(buf, 100);
+
+  mqttPayload = buf;
+  mqttClient.publish(mqttTopic , mqttPayload);
+  if (!mqttClient.connected()) {
+    mqttClient.connect(mac, mqttUser, mqttPass);
+  }
+
+
+}
+
+
+float get_units_kg()
+{
+  return(scale.get_units()*0.453592);
+}
+
+void print_ip_details() {
+  Serial.print("Ethernet Status : ");
+  Serial.println(Ethernet.linkStatus());  
+  Serial.print("IP Address : ");
+  Serial.println(Ethernet.localIP());
+  Serial.print("Subnet mask : ");
+  Serial.println(Ethernet.subnetMask());
+  Serial.print("IP Gateway : ");
+  Serial.println(Ethernet.gatewayIP());
+  Serial.print("DNS : ");
+  Serial.println(Ethernet.dnsServerIP());  
+}
